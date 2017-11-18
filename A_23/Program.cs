1095cdbbad9e3788e9e26d7be751e6cb460c4e86
Piotrek <@>
2017-11-18 14:53:00 +0100

    przeslanie plikow

---
+++ A_23/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wrocław, 637683");
+            Console.WriteLine("Bydgoszcz, 353938");
+            Console.WriteLine("Toruń, 202521");
+            Console.WriteLine("Lublin, 340466");
+            Console.WriteLine("Gorzów Wielkopolski, 123995");
+            Console.WriteLine("Zielona Góra, 139330");
+            Console.WriteLine("Łódź, 696503");
+            Console.WriteLine("Kraków, 765320");
+            Console.WriteLine("m. st. Warszawa, 1753977");
+            Console.WriteLine("Opole, 128043");
+            Console.WriteLine("Rzeszów, 188021");
+            Console.WriteLine("Białystok, 296628");
+            Console.WriteLine("Gdańsk, 463754");
+            Console.WriteLine("Katowice, 298111");
+            Console.WriteLine("Kielce, 197704");
+            Console.WriteLine("Olsztyn, 172993");
+            Console.WriteLine("Poznań, 540372");
+            Console.WriteLine("Szczecin, 404878");
+
+            Console.ReadKey();
+
+        }
+    }
+}
